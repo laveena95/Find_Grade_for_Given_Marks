a68cd0237209e8a83fd57fec4f2b10f0bc0e771d
Laveena <48251610+laveena95@users.noreply.github.com>
2019-05-05 10:37:07 +0530

    Add files via upload

---
+++ if_function.py
@@ -0,0 +1,17 @@
+#if function used to define a grade for a given marks in PYTHON
+marks=85
+if (marks>=75 and marks<=100):
+    print("The Grade is A");
+
+else if (marks>=65 & marks<=74):
+    print("The Grade is B");
+
+else if (marks>=55 & marks<=64):
+    print("The Grade is C");
+
+else if (marks>=40 & marks<=54):
+    print("The Grade is D");
+
+else:
+    print("The Grade is Fail");
+
